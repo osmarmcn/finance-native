b816caac1e9d26ba4c4265f75f0d6fbfe641a3a8
osmarmcn <99884631+osmarmcn@users.noreply.github.com>
2024-12-27 16:06:59 -0300

    crinado o showValue

--- src/components/moviments/index.js
+++ src/components/moviments/index.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 export const Moviments = ({data})=> {
+
+    const [showValue, setShowValue] = useState(false)
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={()=> setShowValue(!showValue)}>
          <Text style={styles.date}> {data.date} </Text>
 
          <View style={styles.content}>
             <Text style={styles.label}> {data.label} </Text>
-            <Text style={data.type === 'receitas' ? styles.value : styles.expense}>
 
-                 {data.type === 'receitas' ? `R$ ${data.value}`: `R$ -${data.value}`} 
-            </Text>
+            {showValue ? (
+                <Text style={data.type === 'receitas' ? styles.value : styles.expense}>
+
+                    {data.type === 'receitas' ? `R$ ${data.value}`: `R$ -${data.value}`} 
+                </Text>
+            ) : (
+                <View style={styles.skeleton}>
+
+                </View>
+            )}
+           
 
            
 
@@ -60,5 +71,12 @@ const styles = StyleSheet.create({
         fontSize:16,
         color:'#ff0000',
         fontWeight:'bold',
+    },
+    skeleton:{
+        width:80,
+        height:10,
+        borderRadius:8, 
+        marginTop:6,
+        backgroundColor:'#dadada',
     }
 })
